b844e6013654d768a8fbf6d100aecb0efb6e6489
wory48 <luhamailbox@gmail.com>
2022-02-01 14:31:25 +0530

    export version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { join } from "path";
+import { readFileSync } from "fs";
+
 export { default as FriesLava } from "./structure/FriesLava";
 export { default as FriesNode } from "./structure/FriesNode"
 export { default as FriesPlayer } from "./structure/FriesPlayer";
@@ -5,3 +8,5 @@ export { default as FriesQueue } from "./structure/FriesQueue";
 export { default as FriesRoutePlanner } from "./structure/FriesRoutePlanner";
 export { default as Utils } from "./utils/Utils";
 export { default as loadTypes } from "./utils/Constants";
+
+export const { version } = JSON.parse(readFileSync(join(__dirname, "..", "package.json"), "utf-8"));

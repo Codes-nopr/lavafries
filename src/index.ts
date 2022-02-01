55af9babb56d9ef7f18372ead97110b744bdab53
wory48 <luhamailbox@gmail.com>
2022-02-01 14:08:22 +0530

    export loadTypes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { default as FriesLava } from "./structure/FriesLava";
-export { default as FriesPlayer } from "./structure/FriesPlayer";
 export { default as FriesNode } from "./structure/FriesNode"
+export { default as FriesPlayer } from "./structure/FriesPlayer";
+export { default as FriesQueue } from "./structure/FriesQueue";
 export { default as FriesRoutePlanner } from "./structure/FriesRoutePlanner";
+export { default as Utils } from "./utils/Utils";
+export { default as loadTypes } from "./utils/Constants";
